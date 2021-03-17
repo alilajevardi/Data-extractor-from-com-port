41d71ba106e63b8bd162da6582031ee5a4922201
Alireza Lajevardipour <alilajevardi@users.noreply.github.com>
2021-03-17 20:33:29 +1100

    Add files via upload

---
+++ scr/SerialCom/Main_frm.Designer.vb
@@ -0,0 +1,264 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Main_frm
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Main_frm))
+        Me.InitBtn = New System.Windows.Forms.Button()
+        Me.ActBtn = New System.Windows.Forms.Button()
+        Me.PortBox = New System.Windows.Forms.ComboBox()
+        Me.SendTxt = New System.Windows.Forms.RichTextBox()
+        Me.RecTxt = New System.Windows.Forms.RichTextBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.closeBtn = New System.Windows.Forms.Button()
+        Me.SerialPort1 = New System.IO.Ports.SerialPort(Me.components)
+        Me.BudBox = New System.Windows.Forms.ComboBox()
+        Me.ParamBtn = New System.Windows.Forms.Button()
+        Me.SavBtn = New System.Windows.Forms.Button()
+        Me.ActBox = New System.Windows.Forms.ComboBox()
+        Me.DescTxt = New System.Windows.Forms.RichTextBox()
+        Me.ParamBox = New System.Windows.Forms.ComboBox()
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        Me.PictureBox2 = New System.Windows.Forms.PictureBox()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBox2, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'InitBtn
+        '
+        Me.InitBtn.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.InitBtn.Location = New System.Drawing.Point(543, 60)
+        Me.InitBtn.Name = "InitBtn"
+        Me.InitBtn.Size = New System.Drawing.Size(91, 32)
+        Me.InitBtn.TabIndex = 0
+        Me.InitBtn.Text = "Init"
+        Me.InitBtn.UseVisualStyleBackColor = True
+        '
+        'ActBtn
+        '
+        Me.ActBtn.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.ActBtn.Location = New System.Drawing.Point(543, 300)
+        Me.ActBtn.Name = "ActBtn"
+        Me.ActBtn.Size = New System.Drawing.Size(91, 43)
+        Me.ActBtn.TabIndex = 1
+        Me.ActBtn.Text = "Send Action Command"
+        Me.ActBtn.UseVisualStyleBackColor = True
+        '
+        'PortBox
+        '
+        Me.PortBox.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList
+        Me.PortBox.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!)
+        Me.PortBox.FormattingEnabled = True
+        Me.PortBox.Location = New System.Drawing.Point(416, 45)
+        Me.PortBox.Name = "PortBox"
+        Me.PortBox.Size = New System.Drawing.Size(121, 28)
+        Me.PortBox.TabIndex = 3
+        '
+        'SendTxt
+        '
+        Me.SendTxt.BackColor = System.Drawing.Color.FromArgb(CType(CType(255, Byte), Integer), CType(CType(192, Byte), Integer), CType(CType(128, Byte), Integer))
+        Me.SendTxt.Location = New System.Drawing.Point(17, 30)
+        Me.SendTxt.Name = "SendTxt"
+        Me.SendTxt.Size = New System.Drawing.Size(373, 43)
+        Me.SendTxt.TabIndex = 4
+        Me.SendTxt.Text = ""
+        '
+        'RecTxt
+        '
+        Me.RecTxt.BackColor = System.Drawing.Color.FromArgb(CType(CType(255, Byte), Integer), CType(CType(192, Byte), Integer), CType(CType(128, Byte), Integer))
+        Me.RecTxt.Location = New System.Drawing.Point(17, 101)
+        Me.RecTxt.Name = "RecTxt"
+        Me.RecTxt.Size = New System.Drawing.Size(373, 420)
+        Me.RecTxt.TabIndex = 5
+        Me.RecTxt.Text = ""
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!)
+        Me.Label1.Location = New System.Drawing.Point(17, 9)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(47, 20)
+        Me.Label1.TabIndex = 6
+        Me.Label1.Text = "Send"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!)
+        Me.Label2.Location = New System.Drawing.Point(17, 81)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(66, 20)
+        Me.Label2.TabIndex = 7
+        Me.Label2.Text = "Recieve"
+        '
+        'closeBtn
+        '
+        Me.closeBtn.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.closeBtn.Location = New System.Drawing.Point(416, 450)
+        Me.closeBtn.Name = "closeBtn"
+        Me.closeBtn.Size = New System.Drawing.Size(218, 32)
+        Me.closeBtn.TabIndex = 8
+        Me.closeBtn.Text = "Close"
+        Me.closeBtn.UseVisualStyleBackColor = True
+        '
+        'SerialPort1
+        '
+        '
+        'BudBox
+        '
+        Me.BudBox.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList
+        Me.BudBox.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!)
+        Me.BudBox.FormattingEnabled = True
+        Me.BudBox.Items.AddRange(New Object() {"9600", "38400", "57600", "115200"})
+        Me.BudBox.Location = New System.Drawing.Point(416, 88)
+        Me.BudBox.Name = "BudBox"
+        Me.BudBox.Size = New System.Drawing.Size(121, 28)
+        Me.BudBox.TabIndex = 9
+        '
+        'ParamBtn
+        '
+        Me.ParamBtn.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.ParamBtn.Location = New System.Drawing.Point(543, 192)
+        Me.ParamBtn.Name = "ParamBtn"
+        Me.ParamBtn.Size = New System.Drawing.Size(91, 60)
+        Me.ParamBtn.TabIndex = 1
+        Me.ParamBtn.Text = "Send Parameter Command"
+        Me.ParamBtn.UseVisualStyleBackColor = True
+        '
+        'SavBtn
+        '
+        Me.SavBtn.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.SavBtn.Location = New System.Drawing.Point(416, 395)
+        Me.SavBtn.Name = "SavBtn"
+        Me.SavBtn.Size = New System.Drawing.Size(218, 32)
+        Me.SavBtn.TabIndex = 10
+        Me.SavBtn.Text = "Start saving data"
+        Me.SavBtn.UseVisualStyleBackColor = True
+        '
+        'ActBox
+        '
+        Me.ActBox.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList
+        Me.ActBox.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!)
+        Me.ActBox.FormattingEnabled = True
+        Me.ActBox.Items.AddRange(New Object() {"CTRL+A", "CTRL+D", "CTRL+E", "CTRL+F", "CTRL+I", "CTRL+K", "CTRL+R", "CTRL+S", "CTRL+T", "CTRL+X"})
+        Me.ActBox.Location = New System.Drawing.Point(416, 307)
+        Me.ActBox.Name = "ActBox"
+        Me.ActBox.Size = New System.Drawing.Size(121, 28)
+        Me.ActBox.TabIndex = 11
+        '
+        'DescTxt
+        '
+        Me.DescTxt.BackColor = System.Drawing.SystemColors.AppWorkspace
+        Me.DescTxt.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.DescTxt.ForeColor = System.Drawing.SystemColors.WindowText
+        Me.DescTxt.Location = New System.Drawing.Point(17, 527)
+        Me.DescTxt.Name = "DescTxt"
+        Me.DescTxt.Size = New System.Drawing.Size(373, 90)
+        Me.DescTxt.TabIndex = 12
+        Me.DescTxt.TabStop = False
+        Me.DescTxt.Text = ""
+        '
+        'ParamBox
+        '
+        Me.ParamBox.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList
+        Me.ParamBox.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!)
+        Me.ParamBox.FormattingEnabled = True
+        Me.ParamBox.Items.AddRange(New Object() {"AE", "AO", "AS", "CC", "CT", "DF", "DS", "HL", "ID", "LL", "MU", "PS", "SL", "SM", "SN", "ST", "SV", "TU", "UN"})
+        Me.ParamBox.Location = New System.Drawing.Point(416, 208)
+        Me.ParamBox.Name = "ParamBox"
+        Me.ParamBox.Size = New System.Drawing.Size(121, 28)
+        Me.ParamBox.TabIndex = 11
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.Image = CType(resources.GetObject("PictureBox1.Image"), System.Drawing.Image)
+        Me.PictureBox1.InitialImage = CType(resources.GetObject("PictureBox1.InitialImage"), System.Drawing.Image)
+        Me.PictureBox1.Location = New System.Drawing.Point(511, 545)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(123, 72)
+        Me.PictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom
+        Me.PictureBox1.TabIndex = 13
+        Me.PictureBox1.TabStop = False
+        '
+        'PictureBox2
+        '
+        Me.PictureBox2.Image = CType(resources.GetObject("PictureBox2.Image"), System.Drawing.Image)
+        Me.PictureBox2.InitialImage = CType(resources.GetObject("PictureBox2.InitialImage"), System.Drawing.Image)
+        Me.PictureBox2.Location = New System.Drawing.Point(416, 545)
+        Me.PictureBox2.Name = "PictureBox2"
+        Me.PictureBox2.Size = New System.Drawing.Size(68, 72)
+        Me.PictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom
+        Me.PictureBox2.TabIndex = 14
+        Me.PictureBox2.TabStop = False
+        '
+        'Main_frm
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.SystemColors.AppWorkspace
+        Me.ClientSize = New System.Drawing.Size(647, 622)
+        Me.Controls.Add(Me.PictureBox2)
+        Me.Controls.Add(Me.PictureBox1)
+        Me.Controls.Add(Me.DescTxt)
+        Me.Controls.Add(Me.ParamBox)
+        Me.Controls.Add(Me.ActBox)
+        Me.Controls.Add(Me.SavBtn)
+        Me.Controls.Add(Me.BudBox)
+        Me.Controls.Add(Me.closeBtn)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.RecTxt)
+        Me.Controls.Add(Me.SendTxt)
+        Me.Controls.Add(Me.PortBox)
+        Me.Controls.Add(Me.ParamBtn)
+        Me.Controls.Add(Me.ActBtn)
+        Me.Controls.Add(Me.InitBtn)
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.Name = "Main_frm"
+        Me.Text = "Telstra Swinburne RF Lab - Luxtron 812 Controller"
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBox2, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents InitBtn As System.Windows.Forms.Button
+    Friend WithEvents ActBtn As System.Windows.Forms.Button
+    Friend WithEvents PortBox As System.Windows.Forms.ComboBox
+    Friend WithEvents SendTxt As System.Windows.Forms.RichTextBox
+    Friend WithEvents RecTxt As System.Windows.Forms.RichTextBox
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents closeBtn As System.Windows.Forms.Button
+    Friend WithEvents SerialPort1 As System.IO.Ports.SerialPort
+    Friend WithEvents BudBox As System.Windows.Forms.ComboBox
+    Friend WithEvents ParamBtn As System.Windows.Forms.Button
+    Friend WithEvents SavBtn As System.Windows.Forms.Button
+    Friend WithEvents ActBox As System.Windows.Forms.ComboBox
+    Friend WithEvents DescTxt As System.Windows.Forms.RichTextBox
+    Friend WithEvents ParamBox As System.Windows.Forms.ComboBox
+    Friend WithEvents PictureBox1 As System.Windows.Forms.PictureBox
+    Friend WithEvents PictureBox2 As PictureBox
+End Class
---
+++ scr/SerialCom/Main_frm.vb
@@ -0,0 +1,323 @@
+﻿Imports System
+Imports System.Threading
+Imports System.IO
+Imports System.IO.Ports
+Imports System.ComponentModel
+
+
+Public Class Main_frm
+    '------------------------------------------------
+    Dim myPort As Array
+    Delegate Sub SetTextCallback(ByVal [text] As String)
+
+    Dim sw As StreamWriter = Nothing
+    'Dim fs As FileStream = Nothing
+    Dim sav As Integer = 0
+
+
+    Private Sub Main_frm_KeyUp(ByVal sender As Object, ByVal e As System.Windows.Forms.KeyEventArgs) Handles Me.KeyUp
+        If (e.Modifiers = Keys.Control) Then
+            'e.KeyCode = Keys.B AndAlso
+            MsgBox(e.KeyValue.ToString)
+            SerialPort1.WriteLine(12)
+        End If
+    End Sub
+
+
+
+
+    '------------------------------------------------
+    Private Sub Form1_Load(sender As System.Object, e As System.EventArgs) Handles MyBase.Load
+
+        myPort = IO.Ports.SerialPort.GetPortNames()
+        PortBox.Items.AddRange(myPort)
+
+        If PortBox.Items.Count > 0 Then
+            PortBox.Text = PortBox.Items.Item(0)
+        End If
+
+        BudBox.Text = BudBox.Items.Item(0)
+
+        ActBtn.Enabled = False
+        ParamBtn.Enabled = False
+
+        Me.KeyPreview = True
+
+    End Sub
+    '------------------------------------------------
+    '------------------------------------------------
+    Private Sub Button1_Click(sender As System.Object, e As System.EventArgs) Handles InitBtn.Click
+        SerialPort1.Close()
+        SerialPort1.PortName = PortBox.Text
+        SerialPort1.BaudRate = BudBox.Text
+        SerialPort1.Open()
+        InitBtn.Enabled = False
+        ActBtn.Enabled = True
+        ParamBtn.Enabled = True
+        closeBtn.Enabled = True
+
+    End Sub
+    '------------------------------------------------
+    Private Sub Button2_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles ActBtn.Click
+
+
+
+        Dim Buffer(1) As Byte
+
+
+        'MsgBox(ComboBox3.SelectedIndex.ToString)
+        If ActBox.SelectedIndex > -1 Then
+            Select Case ActBox.SelectedItem.ToString
+                Case "CTRL+A"
+                    Buffer(0) = &H1
+                Case "CTRL+D"
+                    Buffer(0) = &H4
+                Case "CTRL+E"
+                    Buffer(0) = &H5
+                Case "CTRL+F"
+                    Buffer(0) = &H6
+                Case "CTRL+I"
+                    Buffer(0) = &H9
+                Case "CTRL+K"
+                    Buffer(0) = &HB
+                Case "CTRL+Q"
+                    Buffer(0) = &H11
+                Case "CTRL+R"
+                    Buffer(0) = &H12
+                Case "CTRL+T"
+                    Buffer(0) = &H14
+                Case "CTRL+S"
+                    Buffer(0) = &H13
+                Case "CTRL+X"
+                    Buffer(0) = &H18
+            End Select
+            SerialPort1.Write(Chr(Buffer(0)) + vbCr)
+            ActBox.SelectedIndex = -1
+        End If
+
+
+
+
+
+
+
+
+    End Sub
+
+    Private Sub Button4_Click(sender As System.Object, e As System.EventArgs) Handles closeBtn.Click
+
+        If (sw IsNot Nothing) Then
+            'Dim result As DialogResult = MessageBox.Show("Data have not been saved. Do you want to proceed?", "Warning", MessageBoxButtons.YesNo)
+            'If result = DialogResult.No Then
+            '    Return
+            'End If
+            sw.Close()
+        End If
+        SerialPort1.Close()
+        InitBtn.Enabled = True
+        ActBtn.Enabled = False
+        closeBtn.Enabled = False
+        Me.Close()
+
+    End Sub
+
+    Private Sub SerialPort1_DataReceived(sender As System.Object, e As System.IO.Ports.SerialDataReceivedEventArgs) Handles SerialPort1.DataReceived
+        'ReceivedText(SerialPort1.ReadExisting())
+        ReceivedText(SerialPort1.ReadLine())
+    End Sub
+
+    Private Sub ReceivedText(ByVal [text] As String) 'input from ReadExisting
+        Dim strdata As String = ""
+        If Me.RecTxt.InvokeRequired Then
+            Dim x As New SetTextCallback(AddressOf ReceivedText)
+            Me.Invoke(x, New Object() {(text)})
+        Else
+            'Me.RecTxt.Text &= DateAndTime.TimeString.ToString & ":"
+            strdata = [text].ToString
+            Me.RecTxt.Text &= strdata 'append text
+
+
+        End If
+
+        If sav = 1 Then
+            If strdata IsNot "" Then
+                strdata = strdata.Replace(" ", ",")
+                strdata = strdata.Replace(",,,", ",")
+                strdata = strdata.Replace(",,", ",")
+                sw.Write(DateAndTime.TimeString.ToString & " " & strdata)
+                sw.Flush()
+            End If
+
+        End If
+    End Sub
+
+
+
+
+
+    Private Sub Button3_Click(ByVal sender As Object, ByVal e As System.EventArgs) Handles ParamBtn.Click
+
+        SerialPort1.Write(Chr(27) + SendTxt.Text + vbCr)
+
+    End Sub
+
+
+
+
+
+    Private Sub Button5_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles SavBtn.Click
+
+
+
+        Dim saveFileDialog1 As New SaveFileDialog()
+        Dim Path1 As String
+        Dim myStream As Stream
+
+        Path1 = Directory.GetCurrentDirectory().ToString
+
+
+
+        saveFileDialog1.Filter = "CSV Files (*.csv)|*.csv"
+        saveFileDialog1.InitialDirectory = Path1
+        saveFileDialog1.RestoreDirectory = True
+        saveFileDialog1.FileName = System.DateTime.Now.ToString("yyyyMMdd_HHmmss") & "_T_data"
+
+        If saveFileDialog1.ShowDialog = Windows.Forms.DialogResult.OK Then
+            myStream = saveFileDialog1.OpenFile()
+            myStream.Close()
+            Path1 = saveFileDialog1.FileName
+            sw = My.Computer.FileSystem.OpenTextFileWriter(Path1, True)
+        End If
+        sw.WriteLine("@@@ Start to log temperature @@@")
+        sw.WriteLine("Time" & "," & "Channel" & "," & "T_data" & "," & "Unit")
+        'sw.Write(Me.RecTxt.Text)
+        sw.Flush()
+
+
+        'Dim strFile As String = saveFileDialog1.FileName.ToString 'Path1 & "\T_data" & ".csv"
+        'sw = File.AppendText(strFile)
+        'sw.WriteLine("Time" & "," & "Date" & "," & "Data" + vbCrLf)
+
+        'If (Not File.Exists(strFile)) Then
+        '    Try
+        '        fs = File.Create(strFile)
+        '        sw = File.AppendText(strFile)
+        '        sw.WriteLine("Start to log temperature")
+
+        '    Catch ex As Exception
+        '        MsgBox(strFile)
+        '        MsgBox(ex.ToString)
+        '        End
+        '    End Try
+        'Else
+        '    sw = File.AppendText(strFile)
+        '    sw.WriteLine("Time" & "," & "Date" & "," & "Data")
+
+        'End If
+        sav = 1
+
+
+
+
+    End Sub
+
+
+
+
+
+    Private Sub ComboBox4_SelectionChangeCommitted(ByVal sender As Object, ByVal e As System.EventArgs) Handles ParamBox.SelectionChangeCommitted
+        Dim description As String
+
+
+        If ParamBox.SelectedIndex > -1 Then
+            Select Case ParamBox.SelectedItem.ToString
+                Case "AE"
+                    description = "Enable/Disable Analog Outputs" & Environment.NewLine & "AE = ENABLED | DISABLED"
+                Case "AO"
+                    description = "Set Analog-Output Temperature Offset Factor in degrees" & Environment.NewLine & "AO = -30 oC"
+                Case "AS"
+                    description = "Set Analog-Output Temperature Scaling Factor in degrees"
+                Case "CC"
+                    description = "Enable/Disable Application of Calibration Factors in degrees" & Environment.NewLine & "CC = ENABLED | DISABLED"
+                Case "CT"
+                    description = "Set Calibration Temperature for Auto-Calibration Function in degrees" & Environment.NewLine & "CT = 50.00"
+                Case "DF"
+                    description = "Set Serial Data Output Format" & Environment.NewLine & "DF = FULL | ABBR"
+                Case "DS"
+                    description = "Capture User Data Sequence"
+                Case "HL"
+                    description = "Set High Temperature Limit (obsolete, no longer supported)"
+                Case "LL"
+                    description = "Set Low Temperature Limit (obsolete, no longer supported)"
+                Case "MU"
+                    description = "Set Interval Between Temperature Measurement Reports" & Environment.NewLine & "1/MU = reporting frequency. Continuous 'C' mode reporting sets the reporting interval to the minimum practical value (4Hz). If 2 channels are active MU=C would yield 250mS * 2 = 0.5 S. Intervals: 0.25 - 600 S or 1 - 10 M."
+                Case "PS"
+                    description = "Specify Active-Input List" & Environment.NewLine & "PS = 1,2"
+                Case "SL"
+                    description = "Report LED Light Source Intensity Level" & Environment.NewLine & "SL = 1: 1.81 %     2: 2.66 %"
+                Case "SM"
+                    description = "Set Number of Samples to be Retained Per Measurement" & Environment.NewLine & "The number of samples per second per channel is 4Hz divided by the number of active channels."
+                Case "SN"
+                    description = "Report Device's Serial Number"
+                Case "ST"
+                    description = "Select Startup Mode" & Environment.NewLine & "ST = ENABLED | DISABLED --> Standard mode | Standby mode" & Environment.NewLine & "Standard mode: reporting starts automatically | Standby mode: reporting must be started manually"
+                Case "SV"
+                    description = "Save Current User Parameters and Calibration Factors" & Environment.NewLine & "The SV command is used to copy the currently active user parameter values and input-channel calibration factors to nonvolatile (NvRam) storage for use in subsequent sessions." & Environment.NewLine & "!!!! Important for Calibration !!!!"
+                Case "TU"
+                    description = "Reports the Currently Selected Look-up Table"
+                Case "UN"
+                    description = "Specify Output Units of Measure" & Environment.NewLine & "UN = CELSIUS | KELVIN | FAHRENHEIT"
+            End Select
+            DescTxt.Text = description
+            SendTxt.Text = ParamBox.SelectedItem.ToString & " ?"
+
+        End If
+        ActBox.SelectedIndex = -1
+    End Sub
+
+    Private Sub ComboBox3_SelectionChangeCommitted(ByVal sender As Object, ByVal e As System.EventArgs) Handles ActBox.SelectionChangeCommitted
+        Dim description As String
+
+
+        If ActBox.SelectedIndex > -1 Then
+            Select Case ActBox.SelectedItem.ToString
+                Case "CTRL+A"
+                    description = "Abort calibration sequence in progress"
+                Case "CTRL+D"
+                    description = "Disable remote control of temperature reporting"
+                Case "CTRL+E"
+                    description = "Enable remote control of temperature reporting"
+                Case "CTRL+F"
+                    description = "Flush the measurement buffer"
+                Case "CTRL+I"
+                    description = "Begin data sampling"
+                Case "CTRL+K"
+                    description = "Begin auto-calibration sequence"
+                Case "CTRL+Q"
+                    description = "Report the most recent measurement / enable serial output"
+                Case "CTRL+R"
+                    description = "Start standard run mode"
+                Case "CTRL+T"
+                    description = "Enter standby mode"
+                Case "CTRL+S"
+                    description = "Disable serial data output"
+                Case "CTRL+X"
+                    description = "Reset the instrument"
+            End Select
+            SendTxt.Text = ActBox.SelectedItem.ToString
+            DescTxt.Text = description
+        End If
+        ParamBox.SelectedIndex = -1
+    End Sub
+
+
+    Private Sub ComboBox1_SelectedIndexChanged(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles PortBox.SelectedIndexChanged
+        InitBtn.Enabled = True
+    End Sub
+
+    Private Sub Main_frm_Closed(sender As Object, e As EventArgs) Handles Me.Closed
+        If (sw IsNot Nothing) Then
+            sw.Close()
+        End If
+    End Sub
+End Class
